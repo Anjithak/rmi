32d9e58f9fb1d797cde5881a888509ca09a11679
Anjithak <k.anjitha@gmail.com>
2014-09-30 14:59:08 +0530

    initial commit

---
+++ server.java
@@ -0,0 +1,21 @@
+import java.rmi.*;
+import java.rmi.registry.*;
+import java.net.*;
+import java.rmi.server.*;
+public class server 
+{
+  public static void main(String args[]) 
+  {
+    try{
+  
+         //int thisport;
+        // String thisaddress;
+         rmiclass rc=new rmiclass();
+         Registry registry=LocateRegistry.createRegistry(3333);
+        // thisaddress="192.168.0.8";
+        // thisport=3230;
+         registry.rebind("rmiinter",rc);  
+        }
+      catch(Exception e){}
+  }
+}
